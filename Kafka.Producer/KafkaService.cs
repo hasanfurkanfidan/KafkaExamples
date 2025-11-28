d9e59f79f878bb889b7eb4c7df553f1f0e27a802
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2025-11-28 21:57:25 +0300

    Add project files.

---
+++ Kafka.Producer/KafkaService.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+namespace Kafka.Producer
+{
+    public class KafkaService
+    {
+        static string topicName = "mytopic";
+        public async Task CreateTopicAsync()
+        {
+            using var adminClient = new AdminClientBuilder(new AdminClientConfig()
+            {
+                BootstrapServers = "localhost:9094",
+            }).Build();
+
+            try
+            {
+                await adminClient.CreateTopicsAsync(new[]
+                {
+            new TopicSpecification(){Name=topicName,NumPartitions=3,ReplicationFactor=1}
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Kafka.Producer;
+
+Console.WriteLine("Kafka Producer");
+KafkaService kafkaService = new();
+await kafkaService.CreateTopicAsync();
