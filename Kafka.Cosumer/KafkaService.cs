22ac4e5a34a7c6487a0ab67ba8e07b045a1f4896
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2025-11-29 14:07:28 +0300

    updates

--- Kafka.Cosumer/KafkaService.cs
+++ Kafka.Cosumer/KafkaService.cs
@@ -68,6 +68,28 @@ namespace Kafka.Cosumer
                 Console.WriteLine($"gelen mesaj : {consumeResult.Message.Value.UserId} key : {consumeResult.Message.Key}");
             }
         }
+        public async Task ConsumeComplexMessageWithIntKeyAndHeader(string topicName)
+        {
+            var config = new ConsumerConfig()
+            {
+                BootstrapServers = "localhost:9094",
+                GroupId = "use-case-1-group-1",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            var consumer = new ConsumerBuilder<int, OrderCreatedEvent>(config)
+                .SetValueDeserializer(new CustomValueDeserializer<OrderCreatedEvent>())
+                .Build();
+            consumer.Subscribe(topicName);
+
+
+            while (true)
+            {
+                var consumeResult = consumer.Consume();
+               
+                Console.WriteLine($"gelen mesaj : {consumeResult.Message.Value.UserId} key : {consumeResult.Message.Key}");
+            }
+        }
 
     }
 }
--- Kafka.Producer/KafkaService.cs
+++ Kafka.Producer/KafkaService.cs
@@ -1,6 +1,7 @@
 ﻿using Confluent.Kafka;
 using Confluent.Kafka.Admin;
 using Kafka.Producer.Events;
+using System.Text;
 
 namespace Kafka.Producer
 {
@@ -111,6 +112,48 @@ namespace Kafka.Producer
                 }
             }
         }
+        public async Task SendComplexMessageWithIntKeyAndHeader(string topicName)
+        {
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9094"
+            };
+
+            using var producer = new ProducerBuilder<int, OrderCreatedEvent>(config).
+                SetValueSerializer(new CustomValueSerializer<OrderCreatedEvent>()).
+                Build();
+
+            foreach (var item in Enumerable.Range(1, 100))
+            {
+                var orderCreatedEvent = new OrderCreatedEvent
+                {
+                    OrderCode = Guid.NewGuid().ToString(),
+                    TotalPrice = item * 100,
+                    UserId = item
+                };
+
+                var header = new Headers
+                {
+                    { "correlation_id", Encoding.UTF8.GetBytes("123") },
+                    { "version", Encoding.UTF8.GetBytes("v1") }
+                };
+
+                var message = new Message<int, OrderCreatedEvent>()
+                {
+                    Value = orderCreatedEvent,
+                    Key = item,
+                    Headers = header
+                };
+
+                var result = await producer.ProduceAsync(topicName, message);
+
+                foreach (var propertyInfo in result.GetType().GetProperties())
+                {
+                    Console.WriteLine($"{propertyInfo.Name} : {propertyInfo.GetValue(result)}");
+                }
+            }
+        }
+
 
     }
 }
